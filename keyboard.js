31272a76d834369af88db003ee60c5eb29836e7e
r01922002 <r01922002@linux1.csie.ntu.edu.tw>
2013-11-26 00:04:17 +0800

    test

--- keyboard.js
+++ keyboard.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-/*     $("*").addClass('leap-interactive'); */
+    /*     $("*").addClass('leap-interactive'); */
 
     $('#keyboard li').hover(function () {
         $('#enlargeLetter').remove();
@@ -17,9 +17,26 @@ $(document).ready(function () {
             left: $left,
             width: $width,
             height: $height
-        }).click(
-
-        );
+        });
+        
+        
+        $class = $(this).attr('class');
+        /*         console.log($class); */
+        $enlargeLetter2 = '<li id="enlargeLetter2" class="' + $class + ' leap-interactive tapper"' + ">" + $(this).html() + "</li>";
+        $($enlargeLetter2).insertAfter(this);
+        $left = $(this).position().left - 10;
+        $top = $(this).position().top - 10+90;
+        $width = $(this).width() + 20;
+        $height = $(this).height() + 20;
+        //        console.log("left "+$left+" top "+$top);
+        $('#enlargeLetter2').css({
+            top: $top,
+            left: $left,
+            width: $width,
+            height: $height
+        });
+        
+        
         document.getElementById('enlargeLetter').addEventListener("click", function (e) {
             writeWord();
         });
@@ -36,130 +53,133 @@ $(document).ready(function () {
         enableGestures: true
     };
 
-/* 	printKeyPosition(); */
-		var keys = $('#keyboard li');
-      Leap.loop(controllerOptions, function (frame) {
-        
-        if (frame.pointables.length > 0) {
-                            for (var i = 0; i < frame.pointables.length; i++) {
-                                      var pointable = frame.pointables[i];
+    /* 	printKeyPosition(); */
+    var keys = $('#keyboard li');
+    Leap.loop(controllerOptions, function (frame) {
 
-                                      if (pointable.tool) {
-                                        
-                                      }
-                                      else {
-                                        if(i==0){
-                                        
-										  
-										  var left, right, top, bottom, rowStartIndex, rowEndIndex;
-										  var leapPosition = leapToScene(frame,pointable.tipPosition);
-										  $leapPosition = leapToScene(frame,pointable.tipPosition);
-										  $('#pos').html('x '+Math.round(leapPosition[0])+' y '+Math.round(leapPosition[1]));
-										  $('#cursor').css({left:$leapPosition[0],top:$leapPosition[1], position:'absolute'});
-										 
-/* 										  console.log(leapPosition); */
-										  if(leapPosition[1] >=102 && leapPosition[1] <=190){
-											  rowStartIndex = 0;
-											  rowEndIndex = 13;
-										  }else if(leapPosition[1] >=191 && leapPosition[1] <=279){
-											  rowStartIndex = 14;
-											  rowEndIndex = 27;
-										  }else if(leapPosition[1] >=280 && leapPosition[1] <=368){
-											  rowStartIndex = 28;
-											  rowEndIndex = 40;
-										  }else if(leapPosition[1] >=369 && leapPosition[1] <=457){
-											  rowStartIndex = 41;
-											  rowEndIndex = 52;
-										  }else{
-											  rowStartIndex = 53;
-											  rowEndIndex = 53;
-										  }
-										  for(var j=rowStartIndex ;j<=rowEndIndex ;j++){
-										  	  left = keys.eq(j).position().left;
-										  	  if(j!=rowEndIndex){
-												  	right = keys.eq(j+1).position().left-1;
-										  	  }else{
-											  	  //the last key of this row
-											  	  right = 1286;
-										  	  }
-										  	  top = keys.eq(j).position().top;
-										  	  bottom = keys.eq(j).position().top + 89 -1;
-										  	  
-										  	 
-										  	  
-											  if(leapPosition[0] >= left && leapPosition[0] <= right && leapPosition[1] <= bottom && leapPosition[1] >= top){
-											  /*
+        if (frame.pointables.length > 0) {
+            for (var i = 0; i < frame.pointables.length; i++) {
+                var pointable = frame.pointables[i];
+
+                if (pointable.tool) {
+
+                } else {
+                    if (i == 0) {
+
+
+                        var left, right, top, bottom, rowStartIndex, rowEndIndex;
+                        var leapPosition = leapToScene(frame, pointable.tipPosition);
+                        $leapPosition = leapToScene(frame, pointable.tipPosition);
+                        $('#pos').html('x ' + Math.round(leapPosition[0]) + ' y ' + Math.round(leapPosition[1]));
+                        $('#cursor').css({
+                            left: $leapPosition[0],
+                            top: $leapPosition[1],
+                            position: 'absolute'
+                        });
+
+                        /* 										  console.log(leapPosition); */
+                        if (leapPosition[1] >= 102 && leapPosition[1] <= 190) {
+                            rowStartIndex = 0;
+                            rowEndIndex = 13;
+                        } else if (leapPosition[1] >= 191 && leapPosition[1] <= 279) {
+                            rowStartIndex = 14;
+                            rowEndIndex = 27;
+                        } else if (leapPosition[1] >= 280 && leapPosition[1] <= 368) {
+                            rowStartIndex = 28;
+                            rowEndIndex = 40;
+                        } else if (leapPosition[1] >= 369 && leapPosition[1] <= 457) {
+                            rowStartIndex = 41;
+                            rowEndIndex = 52;
+                        } else {
+                            rowStartIndex = 53;
+                            rowEndIndex = 53;
+                        }
+                        for (var j = rowStartIndex; j <= rowEndIndex; j++) {
+                            left = keys.eq(j).position().left;
+                            if (j != rowEndIndex) {
+                                right = keys.eq(j + 1).position().left - 1;
+                            } else {
+                                //the last key of this row
+                                right = 1286;
+                            }
+                            top = keys.eq(j).position().top;
+                            bottom = keys.eq(j).position().top + 89 - 1;
+
+
+
+                            if (leapPosition[0] >= left && leapPosition[0] <= right && leapPosition[1] <= bottom && leapPosition[1] >= top) {
+                                /*
 console.log(keys.eq(j).html());
 											  console.log(keys.eq(j).position());
 */
-keys.eq(j-1).mouseleave();
-												  keys.eq(j).mouseenter();
-											  }
-/* 											  console.log('j '+j+' left '+left+' right '+right+' top '+top+' bottom '+bottom+' x '+leapPosition[0]+' y '+leapPosition[1]); */
-										  }
-										  on = false;
-										  
-/*                                           console.log(leapToScene(frame,pointable.tipPosition)+"\n"); */
-                                          }
-                                      }
-                                     
-                                      
+                                keys.eq(j - 1).mouseleave();
+                                keys.eq(j).mouseenter();
                             }
+                            /* 											  console.log('j '+j+' left '+left+' right '+right+' top '+top+' bottom '+bottom+' x '+leapPosition[0]+' y '+leapPosition[1]); */
                         }
+                        on = false;
 
-                        for( var i =  0; i < frame.gestures.length; i++){
+                        /*                                           console.log(leapToScene(frame,pointable.tipPosition)+"\n"); */
+                    }
+                }
 
-                          var gesture  = frame.gestures[0];
-                          //Per gesture code goes here
-                          var type = gesture.type;
-                          console.log("gesture: "+type+"\n");
-          
-                          switch( type ){
 
-                            case "circle":
-                              //onCircle( gesture );
+            }
+        }
 
-                              break;
-                              
-                            case "swipe":
-                              //onSwipe( gesture );
-                              
-                              break;
+        for (var i = 0; i < frame.gestures.length; i++) {
 
-                            case "screenTap":
-                              //onScreenTap( gesture );
-                              
-                              break;
+            var gesture = frame.gestures[0];
+            //Per gesture code goes here
+            var type = gesture.type;
+            console.log("gesture: " + type + "\n");
 
-                            case "keyTap":
-                              //onKeyTap( gesture );
-                              writeWord();
-                              break;
+            switch (type) {
 
-                          }
-                        }
-        
-        
+            case "circle":
+                //onCircle( gesture );
+
+                break;
+
+            case "swipe":
+                //onSwipe( gesture );
+
+                break;
+
+            case "screenTap":
+                //onScreenTap( gesture );
+
+                break;
+
+            case "keyTap":
+                //onKeyTap( gesture );
+                writeWord();
+                break;
+
+            }
+        }
+
+
         //screenTap distance
-        	if (lastFrame !== null) {
-/*             console.log(frame.pointables[0].touchDistance); */
-            	for (var p = 0; p < frame.pointables.length; p++) {
-                	if (frame.pointables[p].touchDistance <= 0 && lastFrame.pointables[p].touchDistance > 0) {
-                    	writeWord();
-						}
-					}
-				}
+        if (lastFrame !== null) {
+            /*             console.log(frame.pointables[0].touchDistance); */
+            for (var p = 0; p < frame.pointables.length; p++) {
+                if (frame.pointables[p].touchDistance <= 0 && lastFrame.pointables[p].touchDistance > 0) {
+                    writeWord();
+                }
+            }
+        }
         lastFrame = frame;
-            });
+    });
 
-			
+
 
     $('#q').mouseenter();
 
-});   //end of document.ready
+}); //end of document.ready
 
 function writeWord() {
-	var $writeBox = $('#write'),
+    var $writeBox = $('#write'),
         shift = false,
         capslock = false;
     var $this = $('#enlargeLetter'),
@@ -208,43 +228,43 @@ function writeWord() {
     $writeBox.html($writeBox.html() + charater);
 }
 
-function leapToScene( frame , leapPos ){
+function leapToScene(frame, leapPos) {
 
     var iBox = frame.interactionBox;
 
-    var left = iBox.center[0] - iBox.size[0]/2*2;
-    var top = iBox.center[1] + iBox.size[1]/2*2;
+    var left = iBox.center[0] - iBox.size[0] / 2 * 2;
+    var top = iBox.center[1] + iBox.size[1] / 2 * 2;
 
     var x = leapPos[0] - left;
     var y = leapPos[1] - top;
 
     //console.log(iBox.size[0]+" "+iBox.size[1]);
-    x /= (iBox.size[0]*2);
-    y /= (iBox.size[1]*2);
+    x /= (iBox.size[0] * 2);
+    y /= (iBox.size[1] * 2);
 
-    if(x < 0)
-      x = 0;
-    else if(x > 1)
-      x = 1;
+    if (x < 0)
+        x = 0;
+    else if (x > 1)
+        x = 1;
 
-    if(y < -1)
-      y = -1;
-    else if(y > 0)
-      y = 0;
+    if (y < -1)
+        y = -1;
+    else if (y > 0)
+        y = 0;
 
     // canvs area
     x *= 1286; //width
     y *= 445; //height
     y -= 102;
 
-    return [ x , -y ];
+    return [x, -y];
 
 }
 
-function printKeyPosition(){
-	for(i=0 ;i<$('#keyboard li').length ;i++){
-		console.log('i '+i);
-		console.log($('#keyboard li').eq(i).position());
-		console.log($('#keyboard li').eq(i).html());
-	}
+function printKeyPosition() {
+    for (i = 0; i < $('#keyboard li').length; i++) {
+        console.log('i ' + i);
+        console.log($('#keyboard li').eq(i).position());
+        console.log($('#keyboard li').eq(i).html());
+    }
 }
